e320bbd41f37c4f034617930cc4ba18cc0ff3ff9
Joseph H. Lee <forkjoseph@gmail.com>
2016-12-07 20:14:19 -0500

    more sql statements

--- src/benchmark/github/client.java
+++ src/benchmark/github/client.java
@@ -41,44 +41,48 @@ public class client {
 
    public static void main(String[] args) {
      Connection conn = null;
-     String sql1 = "SELECT * FROM output "; 
-     // WHERE STATE='" + sql_state + "'";
-     // String sql2 = sql1 + "AND ENROLLMENT >= 1000";
-     // String sql3 = sql2 + "AND END_GRADE=12";
-     try {
-       conn = getConn();
-       System.out.println("Creating statement...");
+     String[] sqls = { 
+      "SELECT * FROM output ", 
+      "SELECT * FROM output WHERE user_email like '%.com'",
+      "SELECT * FROM output WHERE user_email like '%.edu'"
+     };
+     for (int k = 0; k < sqls.length; k++) 
+     {
+       try {
+         conn = getConn();
+         System.out.println("Executing statement: " + sqls[k]);
 
-       long startTime = System.currentTimeMillis();
-       ResultSet rs = execSql(conn, sql1);
-       long estimatedTime = System.currentTimeMillis() - startTime;
-       ResultSetMetaData rsmd = rs.getMetaData();
-       int colNum = rsmd.getColumnCount();
-       int counter = 0;
+         long startTime = System.currentTimeMillis();
+         ResultSet rs = execSql(conn, sqls[k]);
+         long estimatedTime = System.currentTimeMillis() - startTime;
+         ResultSetMetaData rsmd = rs.getMetaData();
+         int colNum = rsmd.getColumnCount();
+         int counter = 0;
 
-       while (rs.next()) {
-         // for (int i = 1; i <= colNum; i++) {
-         //   if (i != 1)
-         //     System.out.print("\t | ");
-         //   System.out.print(rs.getObject(i));
-         // }
-         counter++;
-         // System.out.println();
-       }
+         while (rs.next()) {
+           // for (int i = 1; i <= colNum; i++) {
+           //   if (i != 1)
+           //     System.out.print("\t | ");
+           //   System.out.print(rs.getObject(i));
+           // }
+           counter++;
+           // System.out.println();
+         }
 
-       System.out.println("Time: " + estimatedTime + " ms");
+         System.out.println("Time: " + estimatedTime + " ms");
 
-       rs.close();
-       conn.close();
-      } catch (SQLException se) {
-        se.printStackTrace();
-      } finally {
-        try{
-           if(conn!=null)
-              conn.close();
-        } catch(SQLException se) {
-           se.printStackTrace();
+         rs.close();
+         conn.close();
+        } catch (SQLException se) {
+          se.printStackTrace();
+        } finally {
+          try{
+             if(conn!=null)
+                conn.close();
+          } catch(SQLException se) {
+             se.printStackTrace();
+          }
         }
-      }
+     }
    }
 }
